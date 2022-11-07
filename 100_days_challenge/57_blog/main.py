7c951c6f1532ba4029cdbc1d756747038a35a0bc
Jakub Chojnacki <jacobchojnacki@protonmail.com>
2022-11-07 16:33:14 +0100

    daily checkpoint

---
+++ 100_days_challenge/57_blog/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import requests
+from post import Post
+from postDemo import PostDemo
+
+app = Flask(__name__)
+posts = requests.get("https://api.npoint.io/b62d550c471081c3a23e").json()
+post_demo_objects = []
+for post in posts:
+    post_demo_objects.append(PostDemo(post["title"], post["subtitle"]))
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", postDemo=post_demo_objects)
+
+
+@app.route('/post/<num>')
+def get_post(num):
+    response = requests.get(f"https://api.npoint.io/b62d550c471081c3a23e/{num}")
+    id = response.json()["id"]
+    title = response.json()["title"]
+    subtitle = response.json()["subtitle"]
+    body = response.json()["body"]
+    post = Post(id, title, subtitle, body)
+    return render_template("post.html", post=post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ 100_days_challenge/57_blog/postDemo.py
@@ -0,0 +1,4 @@
+class PostDemo:
+    def __init__(self, title, subtitle):
+        self.title = title
+        self.subtitle = subtitle
